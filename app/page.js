131926e488aa25f9c667825b35382c7fd100dc26
mick243 <39043252+mick243@users.noreply.github.com>
2026-09-01 13:05:02 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
         </div>
         <About />
         <Skills />
-        <Career />
+     /* <Career /> */
         <Projects />
         <Process />
         <Contact />
